3623dc3c9e64e824b02f26ecbb68ba2e8eb1fe3c
abderrahimx05 <75130312+abderrahimx05@users.noreply.github.com>
2024-11-25 22:24:17 +0100

    page bateau done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { BateauxPage } from './bateaux/bateaux.page';
+import { BateauDetailsPage } from './bateau-details/bateau-details.page';
 const routes: Routes = [
   {
     path: 'home',
@@ -18,6 +19,13 @@ const routes: Routes = [
     path: 'bateaux',
     component: BateauxPage,
   },
+  { path: 'bateaux/:id',
+    component: BateauDetailsPage},
+  {
+    path: 'bateau-details',
+    loadChildren: () => import('./bateau-details/bateau-details.module').then( m => m.BateauDetailsPageModule)
+  },
+
 
 ];
 
