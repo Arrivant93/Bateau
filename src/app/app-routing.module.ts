72f4cc422cd0a5ac549366c5da224338be402683
abderrahimx05 <75130312+abderrahimx05@users.noreply.github.com>
2024-11-25 15:58:55 +0100

    create page bateaux

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'bateaux',
+    loadChildren: () => import('./bateaux/bateaux.module').then( m => m.BateauxPageModule)
   },
+
 ];
 
 @NgModule({
